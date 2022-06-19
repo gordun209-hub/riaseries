6bde5cf8e6d085d4b874fd58028a74ac28631661
gordun209 <ali-han80@hotmail.com>
2022-06-19 14:44:56 +0300

    add vec exc

--- ria_3/src/main.rs
+++ ria_3/src/main.rs
@@ -27,7 +27,8 @@ fn main() {
             println!("{} : {}", line_num, line);
         }
     }
-    array_ex()
+    array_ex();
+    veccy()
 }
 
 fn add_with_lifetimes<'a, 'b>(j: &'b i32, i: &'a i32) -> i32 {
@@ -54,5 +55,50 @@ fn array_ex() {
             sum += a[i];
         });
         println!("\t({:?} = {})", a, sum);
+    }
+}
+
+// Enabling context lines to be printed out with a <Vec<Vec<T>>>
+
+fn veccy() {
+    let ctx_lines = 2;
+    let needle = "oo";
+    let haystack = "\
+        Every face, every shop,
+bedroom window, public-house, and
+dark square is a picture
+feverishly turned--in search of what?
+It is the same with books.
+ What do we seek
+ through millions of pages?";
+    let mut tags: Vec<usize> = vec![];
+    let mut ctx: Vec<Vec<(usize, String)>> = vec![];
+
+    for (i, line) in haystack.lines().enumerate() {
+        if line.contains(needle) {
+            tags.push(i);
+            let v = Vec::with_capacity(2 * ctx_lines + 1);
+            ctx.push(v);
+        }
+    }
+
+    for (i, line) in haystack.lines().enumerate() {
+        for (j, tag) in tags.iter().enumerate() {
+            let lower_bound = tag.saturating_sub(ctx_lines);
+            let upper_bound = tag + ctx_lines;
+
+            if (i >= lower_bound) && (i <= upper_bound) {
+                let line_as_string = String::from(line);
+                let local_ctx = (i, line_as_string);
+                ctx[j].push(local_ctx);
+            }
+        }
+    }
+
+    for local_ctx in ctx.iter() {
+        for &(i, ref line) in local_ctx.iter() {
+            let line_num = i + 1;
+            println!("{} : {} ", line_num, line);
+        }
     }
 }
